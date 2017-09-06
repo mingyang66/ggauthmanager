00f38bdd3e824f6b0594f8433d6d196346b5770f
姚明洋 <yaomy@go-goal.com>
2017-09-06 15:22:15 +0800

    创建权限管理中心项目

---
+++ app/controllers/MainAction.java
@@ -0,0 +1,32 @@
+
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * @Description:TODO
+ * @version 1.0
+ * @since JDK1.7
+ * @author yaomy
+ * @company xxxxxxxxxxxxxx
+ * @copyright (c) 2017 yaomy Co'Ltd Inc. All rights reserved.
+ * @date 2017年9月6日 下午2:47:41
+ */
+public class MainAction extends Controller{
+
+	public static void index(){
+		render();
+	}
+	public static void dashboard(){
+		render();
+	}
+	public static void manageblog(){
+		render();
+	}
+	public static void messages(){
+		render();
+	}
+	public static void reports(){
+		render();
+	}
+}
