723c1cf212cbdd268d112fe49c241eb73f9479d6
姚明洋 <yaomy@go-goal.com>
2017-09-07 18:26:47 +0800

    新增产品线表单页面

---
+++ app/controllers/ProductLineAction.java
@@ -0,0 +1,20 @@
+
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * @Description:TODO
+ * @version 1.0
+ * @since JDK1.7
+ * @author yaomy
+ * @company xxxxxxxxxxxxxx
+ * @copyright (c) 2017 yaomy Co'Ltd Inc. All rights reserved.
+ * @date 2017年9月7日 下午2:04:18
+ */
+public class ProductLineAction extends Controller{
+
+	public static void addProductLineView(){
+		render();
+	}
+}
