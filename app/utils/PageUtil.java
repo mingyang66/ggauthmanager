79118c27d7d4cbab76a09949e31251a563bd482b
yaomy <1393619868@qq.com>
2017-09-09 14:53:02 +0800

    分页完美优化

--- app/utils/PageUtil.java
+++ app/utils/PageUtil.java
@@ -10,35 +10,42 @@ public class PageUtil {
 
 	public static List<String> getShowPages(int currentPage, int maxPage) {
 		List<String> pages = new ArrayList<String>();
-		int n = 5;
-		int j = 0;
-		if(currentPage >= n){
-			j = currentPage - n + 1;
-			if(currentPage == maxPage && j > 1){
-				j--;
+		if(maxPage < 5){
+			for(int i=0;i<maxPage;i++){
+				pages.add(String.valueOf(1+i));
 			}
-			if(currentPage < maxPage) {
-				n = currentPage + 1;
-			} else {
-				n = currentPage;
-			}
-		}
-		if(currentPage <= n) {
-			currentPage = 1;
-		}
-		for(int i=j;i<n;i++){
-			pages.add(String.valueOf(currentPage+i));
-		}
-		int lastPage = Integer.parseInt(pages.get(pages.size()-1));
-		if(lastPage+1 == maxPage) {
-			pages.add(String.valueOf(maxPage));
 		} else {
-			pages.add("...");
-			pages.add(String.valueOf(maxPage));
-		}
-		if(lastPage == maxPage){
-			pages.remove("...");
-			pages.remove(String.valueOf(maxPage));
+			int n = 5;
+			int j = 0;
+			if(currentPage >= n){
+				j = currentPage - n + 1;
+				if(currentPage == maxPage && j > 1){
+					j--;
+				}
+				if(currentPage < maxPage) {
+					n = currentPage + 1;
+				} else {
+					n = currentPage;
+				}
+			}
+			if(currentPage <= n) {
+				currentPage = 1;
+			}
+			for(int i=j;i<n;i++){
+				pages.add(String.valueOf(currentPage+i));
+			}
+			int lastPage = Integer.parseInt(pages.get(pages.size()-1));
+			if(lastPage+1 == maxPage) {
+				pages.add(String.valueOf(maxPage));
+			} else {
+				pages.add("...");
+				pages.add(String.valueOf(maxPage));
+			}
+			if(lastPage == maxPage){
+				pages.remove("...");
+				pages.remove(String.valueOf(maxPage));
+			}
+			
 		}
 		return pages;
 	}
