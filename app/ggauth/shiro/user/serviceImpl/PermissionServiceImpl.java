57a4e642d8f241197780a4e2cd25685891c6dc47
姚明洋 <yaomy@go-goal.com>
2017-09-21 17:59:02 +0800

    -

---
+++ app/ggauth/shiro/user/serviceImpl/PermissionServiceImpl.java
@@ -0,0 +1,31 @@
+
+package ggauth.shiro.user.serviceImpl;
+
+import org.apache.shiro.authz.Permission;
+
+import ggauth.shiro.user.service.PermissionService;
+
+/**
+ * @Description:TODO
+ * @version 1.0
+ * @since JDK1.7
+ * @author yaomy
+ * @company xxxxxxxxxxxxxx
+ * @copyright (c) 2017 yaomy Co'Ltd Inc. All rights reserved.
+ * @date 2017年9月21日 下午5:27:11
+ */
+public class PermissionServiceImpl implements PermissionService{
+
+	@Override
+	public Permission createPermission(Permission permisson) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePermission(Long permissionId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
